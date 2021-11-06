a2f204c3c24c4b8d9dd29444fba3116532355402
Joseph Micla <josephmicla@gmail.com>
2021-11-06 01:46:37 +0000

    added bite-fish.png, created new class BiteFish

---
+++ public/scripts/entities/bite_fish.js
@@ -0,0 +1,8 @@
+class BiteFish extends Fish {
+  constructor(options) {
+    super(options); // Call super to run code inside Fish's consttructor
+    this.imageUri = '/images/bitefish.gif' //Set image
+  }
+
+
+}
